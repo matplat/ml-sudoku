9207adb991eb6e92e95846c863c4eaa0943751ed
Mateusz Platek <matplatekk@gmail.com>
2024-10-01 20:25:34 +0200

    Sudoku class

---
+++ backend/sudoku/__init__.py
@@ -0,0 +1,4 @@
+from backend.sudoku.field import Field
+from backend.sudoku.containers.line import Row, Column
+from backend.sudoku.containers.square import Square
+from backend.sudoku.sudoku import Sudoku
